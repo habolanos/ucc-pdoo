1e7f8cc4323e181d210c7b04ec322b2259f167f0
Harold Adrián Bolaños Rodríguez <haroldadrian@gmail.com>
2025-02-18 19:37:49 -0500

    Update PGM

--- sesion-02/PgmSingleton.java
+++ sesion-02/PgmSingleton.java
@@ -1,1 +1,32 @@
 package co.edu.ucc.harold.singleton;
+
+public class Banco {
+    private static Banco instance;
+    private String nombreBanco;
+
+    private Banco() {
+        nombreBanco = "Banco Central";
+    }
+
+    public static Banco getInstance() {
+        if (instance == null) {
+            instance = new Banco();
+        }
+        return instance;
+    }
+
+    public void mostrarNombre() {
+        System.out.println("Bienvenido al " + nombreBanco);
+    }
+}
+
+// Uso
+public class Main {
+    private String getIdentidad() {
+        return "Harold Adrian Bolaños Rodriguez";
+    }
+    public static void main(String[] args) {
+        Banco banco = Banco.getInstance();
+        banco.mostrarNombre();
+    }
+}
